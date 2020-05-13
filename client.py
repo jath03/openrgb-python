ad8e9df0d9356e9bc19e4953901e50db58d06dcf
jath03 <jackthughes@yahoo.com>
2020-05-13 10:24:02 -0700

    finished parsing device information, some numbers look off though

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 import socket
 import struct
 import threading
-import typing
+from dataclasses import dataclass
 from time import sleep
 
 
@@ -22,7 +22,17 @@ HEADER_SIZE = 16
 class NetworkClient(object):
     def __init__(self, update_callback, address="127.0.0.1", port=1337):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((address, port))
+        for x in range(5):
+            try:
+                self.sock.connect((address, port))
+                break
+            except ConnectionRefusedError:
+                if x < 4:
+                    print("Unable to connect.  Is the OpenRGB SDK server started?")
+                    print("Retrying in 5 seconds...\n")
+                    sleep(5)
+                elif x == 4:
+                    raise
 
         self.listener = threading.Thread(target=self.listen)
         self.listener.start()
@@ -36,8 +46,6 @@ class NetworkClient(object):
 
         # Requesting the number of devices
         self.send_header(0, NET_PACKET_ID_REQUEST_CONTROLLER_COUNT, 0)
-
-        self.send_header(3, NET_PACKET_ID_REQUEST_CONTROLLER_DATA, 0)
 
     def listen(self):
         while True:
@@ -57,12 +65,85 @@ class NetworkClient(object):
                 elif packet_type == NET_PACKET_ID_REQUEST_CONTROLLER_DATA:
                     data = bytearray(packet_size)
                     self.sock.recv_into(data)
-                    buff = struct.unpack("IiH", data[:struct.calcsize("IiH")])
-                    location = struct.calcsize("IiH")
-                    device_type = buff[1]
-                    name = struct.unpack(f"{buff[-1]}s", data[location:(location+buff[-1])])[0].decode()
-                    location += buff[-1]
-                    print(device_type, name)
+                    self.parseDeviceDescription(data)
+            sleep(.2)
+
+    def requestDeviceData(self, device: int):
+        self.send_header(device, NET_PACKET_ID_REQUEST_CONTROLLER_DATA, 0)
+
+    def parseDeviceDescription(self, data):
+        buff = struct.unpack("Ii", data[:struct.calcsize("Ii")])
+        location = struct.calcsize("Ii")
+        device_type = buff[1]
+        metadata = []
+        for x in range(5):
+            location, val = self.parseSizeAndString(data, location)
+            metadata.append(val)
+        buff = struct.unpack("=Hi", data[location:location + struct.calcsize("=Hi")])
+        location += struct.calcsize("=Hi")
+        num_modes = buff[0]
+        active_mode = buff[-1]
+        modes = []
+        for x in range(num_modes):
+            location, val = self.parseSizeAndString(data, location)
+            buff = struct.unpack("iIIIIIIIIH", data[location:location + struct.calcsize("iIIIIIIIIH")])
+            location += struct.calcsize("iIIIIIIIIH")
+            colors = []
+            for x in range(buff[-1]):
+                colors.append(struct.unpack("I", data[location:location + struct.calcsize("I")]))
+                location += struct.calcsize('I')
+            modes.append([val.strip('\x00'), *buff, colors])
+        num_zones = struct.unpack("H", data[location:location + struct.calcsize("H")])[0]
+        location += struct.calcsize("H")
+        zones = []
+        for x in range(num_zones):
+            location, val = self.parseSizeAndString(data, location)
+            buff = struct.unpack("iIIIH", data[location:location + struct.calcsize("iIIIH")])
+            location += struct.calcsize("iIIIH")
+            height, width = 0, 0
+            matrix = [[]]
+            if buff[-1] > 0:
+                height, width = struct.unpack("II", data[location:location + struct.calcsize("II")])
+                location += struct.calcsize("II")
+                matrix = [[] for x in height]
+                for y in range(height):
+                    for x in range(width):
+                        matrix[y][x] = struct.unpack("I", data[location:location + struct.calcsize("I")])
+                        location += struct.calcsize("I")
+            zones.append([val.strip('\x00'), *buff, width, height, matrix])
+        num_leds = struct.unpack("H", data[location:location + struct.calcsize("H")])[0]
+        location += struct.calcsize("H")
+        leds = []
+        for x in range(num_leds):
+            location, name = self.parseSizeAndString(data, location)
+            value = struct.unpack("I", data[location:location + struct.calcsize("I")])[0]
+            location += struct.calcsize("I")
+            leds.append([name.strip("\x00"), value])
+        num_colors = struct.unpack("H", data[location:location + struct.calcsize("H")])[0]
+        location += struct.calcsize("H")
+        colors = []
+        for x in range(num_colors):
+            color = struct.unpack("I", data[location:location + struct.calcsize("I")])[0]
+            location += struct.calcsize("I")
+            colors.append(color)
+        print("Device Information:\n", "\t", "Device type:", device_type, "\n\t", end="")
+        print(*metadata, sep="\n\t")
+        print("Mode Information:\n", "\t", "Number of modes:", num_modes, "\n\t", "Active Mode:", active_mode, "\n\t", end="")
+        print(*modes, sep='\n\t')
+        print("Zone Information:\n", "\t", "Number of zones:", num_zones, "\n\t", end="")
+        print(*zones, sep='\n\t')
+        print("LED Information:\n", "\t", "Number of LEDs:", num_leds, "\n\t", end="")
+        print(*leds, sep="\n\t")
+        print("Color Information:\n", "\t", "Number of Colors:", num_colors, "\n\t", end="")
+        print(*colors, sep="\n\t")
+        print("---------------------------------")
+
+    def parseSizeAndString(self, data, start=0):
+        size = struct.unpack('H', data[start:start + struct.calcsize('H')])[0]
+        start += struct.calcsize("H")
+        val = struct.unpack(f"{size}s", data[start:start + size])[0].decode()
+        start += size
+        return start, val
 
     def send_header(self, device_id: int, packet_type: int, packet_size: int):
         self.sock.send(struct.pack('ccccIII', b'O', b'R', b'G', b'B', device_id, packet_type, packet_size), socket.MSG_NOSIGNAL)
@@ -72,6 +153,10 @@ class OpenRGBClient(object):
     def __init__(self, address="127.0.0.1", port=1337):
         self.comms = NetworkClient(self.callback)
         self.device_num = 0
+        while self.device_num == 0:
+            sleep(.2)
+        for x in range(self.device_num):
+            self.comms.requestDeviceData(x)
 
     def callback(self, device: int, type: int, data):
         if type == NET_PACKET_ID_REQUEST_CONTROLLER_COUNT:
@@ -81,5 +166,3 @@ class OpenRGBClient(object):
 
 if __name__ == "__main__":
     client = OpenRGBClient()
-    sleep(1)
-    print(client.device_num)
