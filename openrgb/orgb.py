68088e31dcd8f4f21a2c02cda1e4c95715a2748d
shawnc722 <45184146+shawnc722@users.noreply.github.com>
2021-02-11 16:59:12 -0500

    Update orgb.py

--- openrgb/orgb.py
+++ openrgb/orgb.py
@@ -307,13 +307,15 @@ class OpenRGBClient(utils.RGBObject):
         '''
         return [device for device in self.devices if device.type == type]
 
-    def get_devices_by_name(self, name: str) -> list[Device]:
+    def get_devices_by_name(self, name: str, exact: bool = True) -> list[Device]:
         '''
         Gets a list of any devices matching the requested name
         
         :param name: the name of the device(s) you want to get
+        :param exact: whether to check for only a precise match or accpet a device that contains name
         '''
-        return [device for device in self.devices if device.name == name]
+        if exact: return [device for device in self.devices if device.name == name]
+        return [device for device in self.devices if name.lower() in device.name.lower()]
     
     def load_profile(self, name: str, directory: str = ''):
         '''
