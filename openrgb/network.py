4fb8aa070d2b003cc22feac6d8f662e0ac4a8d6c
Chris Browet <cbro@semperpax.com>
2021-04-12 19:08:33 +0200

    FIX: trap all connection exceptions

--- openrgb/network.py
+++ openrgb/network.py
@@ -55,7 +55,7 @@ class NetworkClient:
 
         try:
             self.sock.connect((self.address, self.port))
-        except ConnectionRefusedError:
+        except:
             self.sock = None
             raise
 
